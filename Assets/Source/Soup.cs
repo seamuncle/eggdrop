770d73d28db1a752db074ae71f946a34acfcb6e9
matt hilliard <matt.hilliard@gmail.com>
2016-05-07 01:33:36 -0600

    Added Soup to trigger simple game over and simple UI to display it

---
+++ Assets/Source/Soup.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Soup : MonoBehaviour
+{
+
+	public UIWinText winDelegate;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		Destroy (gameObject);
+		winDelegate.StartWin ();
+	}
+
+}
---
+++ Assets/Source/UI/UIWinText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIWinText : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void StartWin ()
+	{
+		gameObject.SetActive (true);
+	}
+}
